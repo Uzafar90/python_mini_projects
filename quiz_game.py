d133f6b180bbe1819f27ce74f222899f8a11340e
“uzafar90” <utbazafar@yahoo.com>
2023-08-27 08:17:45 -0400

    fix some code

--- quiz_game.py
+++ quiz_game.py
@@ -17,53 +17,62 @@ else:
     print("Incorrect answer")
 
 answer = input("What does RAM stand for? ")
+
 if answer.lower() == "Random Access Memory":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does GPU stand for? ")
 if answer.lower() == "Graphic Processing Unit":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does HDD stand for? ")
 if answer.lower() == "Hard Disk Drive":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does SSD stand for? ")
 if answer.lower() == "Solid State Drive":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does USB stand for? ")
 if answer.lower() == "Universal Serial Bus":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does LAN stand for? ")
 if answer.lower() == "Local Area Network":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does WAN stand for? ")
 if answer.lower() == "Wide Area Network":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+
 answer = input("What does BIOS stand for? ")
 if answer.lower() == "Basic Input/Output System":
     print('Correct answer')
     score += 1
 else: 
     print("Incorrect answer")
+    
 answer = input("What does OS stand for? ")
 if answer.lower() == "Operating System":
     print('Correct answer')
